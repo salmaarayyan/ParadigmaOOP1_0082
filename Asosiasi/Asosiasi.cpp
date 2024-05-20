f7b0592dd1ba06e90b92927fc84a1a363f635471
salmaarayyan <salmaarayyan@gmail.com>
2024-05-20 18:01:02 +0700

    membuat class pasien dengan akses modifier public

---
+++ Asosiasi/Asosiasi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Dokter;
+class Pasien { //asosiasi
+public:
+	string nama;
+	vector<Dokter*> daftar_dokter;
+	Pasien(string pNama) :nama(pNama) {
+		cout << "Pasien\"" << nama << "\" ada\n";
+	}
+	~Pasien() {
+		cout << "Pasien\"" << nama << "\" tidak ada\n";
+	}
+	void tambahDokter(Dokter*);
+	void cetakDokter();
+
+};
