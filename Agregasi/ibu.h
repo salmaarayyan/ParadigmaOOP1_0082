8f998afe5455d6c6f16faa30d36dc45f699a1747
salmaarayyan <salmaarayyan@gmail.com>
2024-05-20 14:59:38 +0700

    membuat function tambahanak dan cetakanak

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -6,5 +6,12 @@ public:
     string nama;
     vector<anak*> daftar_anak;
 
-    
-}
+    ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
